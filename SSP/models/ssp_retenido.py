635e36ef954ffc6fee46b6484fd8adde51255411
Pedro Coronel Santiago <coronelsantiagop@gmail.com>
2017-09-07 09:55:57 -0500

    [REF] SSP : Modificar vistas para agregarlo a encuesta de SSP

--- SSP/models/ssp_retenido.py
+++ SSP/models/ssp_retenido.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from datetime import timedelta
+from odoo import api, fields, models
 
 
 class Partner(models.Model):
@@ -7,11 +8,23 @@ class Partner(models.Model):
 
     _inherit = 'res.partner'
 
-    # Add a new column to the res.partner model, by default partners are not
-    # instructors
-    edad = fields.Integer(string="Edad")
-    # vivienda_id = fields.Many2one('res.partner.title', string='Vivienda',
-    #                          ondelete='cascade')
+    fecha_nacimiento = fields.Date(string=u'Fecha_Nac')
+    fecha_actual = fields.Date(default=fields.Date.today)
+    edad = fields.Integer(string="Edad", compute="_calcular_edad")
+
     vivienda_id = fields.Selection(
         [('1', 'Propia'), ('2', 'Rentada'), ('3', 'Prestada'), ('4', 'Hípotecada'), ('5', 'Otro')], 'Tipo')
     otro_vivienda = fields.Char(string='Otro')
+
+    @api.depends('fecha_nacimiento', 'fecha_actual')
+    def _calcular_edad(self):
+        for r in self:
+            if not (r.fecha_nacimiento and r.fecha_actual):
+                continue
+            # diff = (datetime.date.today() - r.fecha_nac).days
+            #r.edad =  int(diff / 365)
+            #start_date = fields.Date.today
+            start_date = fields.Datetime.from_string(r.fecha_nacimiento)
+            end_date = fields.Datetime.from_string(r.fecha_actual)
+            r.edad = (end_date - start_date).days/365
+            #r.edad = 10 * 10
